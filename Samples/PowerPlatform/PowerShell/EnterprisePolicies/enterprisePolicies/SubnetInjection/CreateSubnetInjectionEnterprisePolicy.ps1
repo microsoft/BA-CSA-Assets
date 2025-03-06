ee255fe5f1b7191ba586523bb32bdbd27938a433
Ali Youssefi <Ali.Youssefi@microsoft.com>
2025-03-06 11:39:34 -0600

    added sample for ent policy
    This includes changes to the enterprise policy samples to account for multiple azure subscriptions and to remove a policy from Azure in a new file called RemoveSubnetInjectionEnteprisePoliciesInSubscription

---
+++ Samples/PowerPlatform/PowerShell/EnterprisePolicies/enterprisePolicies/SubnetInjection/CreateSubnetInjectionEnterprisePolicy.ps1
@@ -0,0 +1,112 @@
+﻿# Load the environment script
+. "$PSScriptRoot\..\Common\EnterprisePolicyOperations.ps1"
+. "$PSScriptRoot\ValidateVnetLocationForEnterprisePolicy.ps1"
+
+
+function CreateSubnetInjectionEnterprisePolicy
+{
+     param(
+        [Parameter(
+            Mandatory=$true,
+            HelpMessage="The Policy subscription"
+        )]
+        [string]$subscriptionId,
+
+        [Parameter(
+            Mandatory=$true,
+            HelpMessage="The Policy resource group"
+        )]
+        [string]$resourceGroup,
+
+        [Parameter(
+            Mandatory=$true,
+            HelpMessage="The Policy name"
+        )]
+        [string]$enterprisePolicyName,
+
+        [Parameter(
+            Mandatory=$true,
+            HelpMessage="The Policy location"
+        )]
+        [string]$enterprisePolicylocation,
+
+        [Parameter(
+            Mandatory=$true,
+            HelpMessage="Primary virtual network Id"
+        )]
+        [string]$primaryVnetId,
+
+        [Parameter(
+            Mandatory=$true,
+            HelpMessage="Primary subnet name"
+        )]
+        [string]$primarySubnetName,
+
+        [Parameter(
+            Mandatory=$true,
+            HelpMessage="Secondary virtual network Id"
+        )]
+        [string]$secondaryVnetId,
+
+        [Parameter(
+            Mandatory=$true,
+            HelpMessage="Secondary subnet name"
+        )]
+        [string]$secondarySubnetName  
+
+    )
+
+#$subscriptionId = "175cd2eb-fac9-40e5-856d-77179edacdb8"
+#$resourceGroup = "RG-PrivateVNet-BAE"
+#$enterprisePolicyName = "VNet-BAE-Dev-EUS"
+#$enterprisePolicylocation = "unitedstates"
+#$primaryVnetId = "/subscriptions/175cd2eb-fac9-40e5-856d-77179edacdb8/resourcegroups/RG-PrivateVNet-BAE/providers/Microsoft.Network/virtualNetworks/VNet-BAE-Dev-EUS"
+#$primarySubnetName = "default"
+#$secondaryVnetId = "/subscriptions/175cd2eb-fac9-40e5-856d-77179edacdb8/resourcegroups/RG-PrivateVNet-BAE/providers/Microsoft.Network/virtualNetworks/VNet-BAE-Dev-WUS"
+#$secondarySubnetName = "Subnet-Secondary"
+
+    Write-Host "Logging In..." -ForegroundColor Green
+    $connect = AzureLogin
+    Connect-AzAccount -Subscription $subscriptionId
+    if ($false -eq $connect)
+    {
+        Write-Host "Error Logging In..." -ForegroundColor Red
+        return
+    }
+
+    Write-Host "Logged In..." -ForegroundColor Green
+    #Write-Host $connect. -ForegroundColor Green
+    Write-Host "Creating Enterprise policy..." -ForegroundColor Green
+
+    $primaryVnet = ValidateAndGetVnet -vnetId $primaryVnetId -enterprisePolicylocation $enterprisePolicylocation
+    if ($primaryVnet -eq $null)
+    {
+       Write-Host "Subnet Injection Enterprise policy not created" -ForegroundColor Red
+       return
+    }
+
+    $secondaryVnet = ValidateAndGetVnet -vnetId $secondaryVnetId -enterprisePolicylocation $enterprisePolicylocation
+    if ($secondaryVnet -eq $null)
+    {
+       Write-Host "Subnet Injection Enterprise policy not created" -ForegroundColor Red
+       return
+    }
+
+    $body = GenerateEnterprisePolicyBody -policyType "vnet" -policyLocation $enterprisePolicyLocation -policyName $enterprisePolicyName -primaryVnetId $primaryVnetId -primarySubnetName $primarySubnetName -secondaryVnetId $secondaryVnetId -secondarySubnetName $secondarySubnetName
+
+    $result = PutEnterprisePolicy $resourceGroup $body
+    if ($result -eq $false)
+    {
+       Write-Host "Subnet Injection Enterprise policy not created" -ForegroundColor Red
+       return
+    }
+    Write-Host "Subnet Injection Enterprise policy created" -ForegroundColor Green 
+
+    $policyArmId = "/subscriptions/$subscriptionId/resourceGroups/$resourceGroup/providers/Microsoft.PowerPlatform/enterprisePolicies/$enterprisePolicyName"
+    $policy = GetEnterprisePolicy $policyArmId
+    $policyString = $policy | ConvertTo-Json -Depth 7
+    Write-Host "Policy created"
+    Write-Host $policyString
+
+}
+CreateSubnetInjectionEnterprisePolicy
---
+++ Samples/PowerPlatform/PowerShell/EnterprisePolicies/enterprisePolicies/SubnetInjection/RemoveSubnetInjectionEnterprisePoliciesInSubscription.ps1
@@ -0,0 +1,31 @@
+﻿# Load the environment script
+. "$PSScriptRoot\..\Common\EnterprisePolicyOperations.ps1"
+
+function RemoveSubnetInjectionEnterprisePoliciesInSubscription
+{
+     param(
+        [Parameter(
+            Mandatory=$true,
+            HelpMessage="The subscriptionId"
+        )]
+        [string]$subscriptionId,
+        [Parameter(
+            Mandatory=$true,
+            HelpMessage="The armId"
+        )]
+        [string]$armId
+    )
+
+    Write-Host "Logging In..." -ForegroundColor Green
+    $connect = AzureLogin $subscriptionId
+    if ($false -eq $connect)
+    {
+        return
+    }
+
+    Write-Host "Logged In..." -ForegroundColor Green
+    $status = RemoveEnterprisePolicy $subscriptionId $armId
+    Write-Host $status
+
+}
+RemoveSubnetInjectionEnterprisePoliciesInSubscription
